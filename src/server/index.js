c7a9402614e859fb1f0f1836c10dc0a420b567e1
Murad Tag <murad.tag.97@gmail.com>
2021-09-15 18:45:03 +0200

    Installed Neccesary Libraries

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,22 @@
 // TODO: Configure the environment variables
+const dotenv = require("dotenv");
+dotenv.config();
 
-const mockAPIResponse = require('./mockAPI.js')
+const mockAPIResponse = require("./mockAPI.js");
 
-const PORT = 8081
+const PORT = 8081;
 
 // TODO add Configuration to be able to use env variables
-
 
 // TODO: Create an instance for the server
 // TODO: Configure cors to avoid cors-origin issue
 // TODO: Configure express to use body-parser as middle-ware.
 // TODO: Configure express static directory.
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+app.get("/", function (req, res) {
+	// res.sendFile('dist/index.html')
+	res.sendFile(path.resolve("src/client/views/index.html"));
+});
 // a route that handling post request for new URL that coming from the frontend
 /* TODO:
     1. GET the url from the request body
@@ -34,14 +35,14 @@ app.get('/', function (req, res) {
      }
 */
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+	res.send(mockAPIResponse);
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(PORT, (error) => {
-    if (error) throw new Error(error)
-    console.log(`Server listening on port ${PORT}!`)
-})
+	if (error) throw new Error(error);
+	console.log(`Server listening on port ${PORT}!`);
+});
 
 // TODO: export app to use it in the unit testing
